ba0b9f661970f7ff740024dc5849e4485084684d
alejandro94020423284 <alejandro.pelaez940204@gmail.com>
2021-06-23 17:12:02 -0400

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,30 +5,9 @@
  jQuery('.img-icohover').hide();
 
  jQuery('.img-ico').hover(function () {
-    jQuery( this ).hide();
-    jQuery('.img-icohover').show();
+     var id=this.id;
+     var idhover=id.replace("img-ico-","img-icohover-")
+
+    jQuery('#'+idhover).show();
     
  })
-
-
-
-
-
-
-
-
-//  var card=document.getElementsByClassName("img-icohover")[0];
-//  function mostrar(event){
-//    if(!event.currentTarget.classList.contains("is-flipped"))
-//    event.currentTarget.classList.add("is-flipped");
-//  /* card.classList.add('is-flipped'); */
-//  }
-   
-
-
-//  const ocultar=(event)=>{
-//    event.currentTarget.classList.remove("is-flipped");
-//  } 
-
-
-    
