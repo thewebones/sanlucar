0492c4e0be6c4f10c42801f345fe0e44ae4894eb
kmilocollado <kmilocollado@gmail.com>
2021-06-24 15:52:24 -0400

    card hover

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,22 @@
  * Created by pompi on 23/06/21.
  */
     
- jQuery('.img-icohover').hide();
+ jQuery('.img-icohover').css("display", "none"); 
 
- jQuery('.img-ico').hover(function () {
+ jQuery('.item').hover(function () {
      var id=this.id;
-     var idhover=id.replace("img-ico-","img-icohover-")
+     var id = id.replace("item-","");
+     jQuery('#img-ico-'+id).css("display", "none");
+     jQuery('#img-icohover-'+id).css("display", "block");
+     jQuery('#item-nombre-'+id).css('color','#8BDDFC')
+ }, function () {
+    var id=this.id;
+    var id = id.replace("item-","");
+    jQuery('#img-ico-'+id).css("display", "block");
+    jQuery('#img-icohover-'+id).css("display", "none");
+    jQuery('#item-nombre-'+id).css('color','#021B5E')
+ }
+ 
+ )
 
-    jQuery('#'+idhover).show();
-    
- })
+
--- template-parts/blocks/app-home/index.php
+++ template-parts/blocks/app-home/index.php
@@ -3,17 +3,17 @@
             <div class="">
             </div>
         </div>
-        <div class="card col-6">
+        <div class="card col-6 text-center">
             
             <?php 
             $count = 1;
                 if(get_field("repeater-home")){
                 foreach (get_field("repeater-home") as $item) {?>
-                    <div class="item">
-                        <a class="item-enlace" href="<?php echo $item["enlace-card"]?>">
-                            <div id="img-ico-<?php $count ?>" class="img-ico text-center" style="background-image: url('<?php echo $item["ico-card"] ?>'); height:50px; width:75px; background-repeat: no-repeat;  background-position: center top"></div>
-                            <div id="img-icohover-<?php $count ?>" class="img-icohover text-center" style="background-image: url('<?php echo $item["ico-card_hover"] ?>'); height:50px; width:75px; background-repeat: no-repeat;  background-position: center top"></div>
-                            <span class="item-nombre mb-3"><?php echo $item["nombre-card"] ?></span>
+                    <div class="item" id="item-<?php echo $count ?>">
+                        <a class="item-enlace" href="<?php echo $item["enlace-card"]?>" target="blank">
+                            <div id="img-ico-<?php echo $count ?>" class="img-ico text-center" style="background-image: url('<?php echo $item["ico-card"] ?>'); height:50px; width:75px; background-repeat: no-repeat;  background-position: center top"></div>
+                            <div id="img-icohover-<?php echo $count ?>" class="img-icohover text-center" style="background-image: url('<?php echo $item["ico-card_hover"] ?>'); height:50px; width:75px; background-repeat: no-repeat;  background-position: center top"></div>
+                            <h6 id="item-nombre-<?php echo $count ?>" class="item-nombre mb-3"><?php echo $item["nombre-card"] ?></h6>
                         </a>
                     </div>
                     
